1c42d09ab90bb1029eb130ef957e93d5c42e0b09
hnishi <hiroshi.nishigami@access-company.com>
2023-09-24 10:42:27 +0900

    Add find_doc_files

--- scripts/embed_documents.py
+++ scripts/embed_documents.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 from langchain.document_loaders import TextLoader
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -6,7 +7,18 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Qdrant
 
 
-def embed_document(document_path: str, qdrant_path: str) -> str:
+def find_doc_files(path: str) -> list[str]:
+    extensions = (".txt", ".md", ".mdx")
+
+    return [
+        os.path.join(r, fn)
+        for r, ds, fs in os.walk(path)
+        for fn in fs
+        if fn.endswith(extensions)
+    ]
+
+
+def embed_document(document_path: str, qdrant_path: str, collection_name: str):
     loader = TextLoader(document_path)
     documents = loader.load()
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
@@ -18,7 +30,7 @@ def embed_document(document_path: str, qdrant_path: str) -> str:
         docs,
         embeddings,
         path=qdrant_path,
-        collection_name="my_documents",
+        collection_name=collection_name,
         # the collection is going to be reused if it already exists
         # https://python.langchain.com/docs/integrations/vectorstores/qdrant#recreating-the-collection
         # force_recreate=False,
@@ -47,9 +59,22 @@ def parse_args():
         type=str,
         default="./local_qdrant",
     )
+    parser.add_argument(
+        "--recursive",
+        help="recursively search for files in the document-path directory",
+        type=bool,
+        default=False,
+    )
+    parser.add_argument(
+        "--collection-name",
+        help="collection name for qdrant",
+        type=str,
+        default="my_documents",
+    )
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_args()
-    embed_document(args.document_path, args.qdrant_path)
+    # embed_document(args.document_path, args.qdrant_path, args.collection_name)
+    print(find_doc_files(args.document_path))
