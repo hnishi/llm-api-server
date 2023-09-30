43715b1b34c8539ba25141cdaeb8fe438e640f24
hnishi <hiroshi.nishigami@access-company.com>
2023-10-01 00:13:48 +0900

    Add directory loader

--- scripts/embed_documents.py
+++ scripts/embed_documents.py
@@ -1,47 +1,62 @@
 import argparse
-import os
 
-from langchain.document_loaders import TextLoader
+from langchain.document_loaders import (
+    DirectoryLoader,
+    TextLoader,
+    UnstructuredMarkdownLoader,
+)
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Qdrant
-
-
-def find_doc_files(path: str) -> list[str]:
-    extensions = (".txt", ".md", ".mdx")
-
-    return [
-        os.path.join(r, fn)
-        for r, ds, fs in os.walk(path)
-        for fn in fs
-        if fn.endswith(extensions)
-    ]
+from qdrant_client import QdrantClient
 
 
 def embed_document(document_path: str, qdrant_path: str, collection_name: str):
-    loader = TextLoader(document_path)
-    documents = loader.load()
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    docs = text_splitter.split_documents(documents)
-
     embeddings = OpenAIEmbeddings()
 
-    qdrant = Qdrant.from_documents(
-        docs,
-        embeddings,
-        path=qdrant_path,
-        collection_name=collection_name,
-        # the collection is going to be reused if it already exists
-        # https://python.langchain.com/docs/integrations/vectorstores/qdrant#recreating-the-collection
-        # force_recreate=False,
-    )
+    create_documents = False
+    qdrant = None
 
-    query = "What did the president say about Ketanji Brown Jackson"
+    if create_documents:
+        txt_docs = DirectoryLoader(
+            document_path, glob="**/*.txt", loader_cls=TextLoader
+        ).load()
+        md_docs = DirectoryLoader(
+            document_path, glob="**/*.md", loader_cls=UnstructuredMarkdownLoader
+        ).load()
+        mdx_docs = DirectoryLoader(
+            document_path, glob="**/*.mdx", loader_cls=UnstructuredMarkdownLoader
+        ).load()
+        raw_docs = txt_docs + md_docs + mdx_docs
+
+        # chunk_size のデフォルトは 4000
+        # https://api.python.langchain.com/en/latest/_modules/langchain/text_splitter.html#CharacterTextSplitter.__init__
+        text_splitter = CharacterTextSplitter(chunk_size=4000, chunk_overlap=0)
+        # text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        docs = text_splitter.split_documents(raw_docs)
+
+        qdrant = Qdrant.from_documents(
+            docs,
+            embeddings,
+            path=qdrant_path,
+            collection_name=collection_name,
+            # the collection is going to be reused if it already exists
+            # https://python.langchain.com/docs/integrations/vectorstores/qdrant#recreating-the-collection
+            force_recreate=True,
+        )
+    else:
+        # https://github.com/qdrant/qdrant-client
+        client = QdrantClient(path=qdrant_path)
+
+        # https://api.python.langchain.com/en/latest/vectorstores/langchain.vectorstores.qdrant.Qdrant.html
+        qdrant = Qdrant(client, collection_name=collection_name, embeddings=embeddings)
+
+    query = "how to load markdown files to langchain?"
     found_docs = qdrant.similarity_search_with_score(query)
 
     # print(found_docs[0])
     document, score = found_docs[0]
-    print(document.page_content)
+    print(document)
     print(f"\nScore: {score}")
 
 
@@ -76,5 +91,4 @@ def parse_args():
 
 if __name__ == "__main__":
     args = parse_args()
-    # embed_document(args.document_path, args.qdrant_path, args.collection_name)
-    print(find_doc_files(args.document_path))
+    embed_document(args.document_path, args.qdrant_path, args.collection_name)
