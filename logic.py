2173685dea2f6671267c2f0c1720f1ba62ba4fc1
hnishi <hiroshi.nishigami@access-company.com>
2023-10-01 14:02:14 +0900

    Add qdrant (#2)

--- logic.py
+++ logic.py
@@ -1,7 +1,15 @@
 from typing import Optional
 
-from langchain.chat_models import ChatOpenAI
 from dotenv import load_dotenv
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.llms import OpenAI
+from langchain.vectorstores import Qdrant
+from qdrant_client import QdrantClient
+
+from constants import QDRANT_PATH
+from models.api import Collection
 
 load_dotenv(verbose=True)
 
@@ -11,10 +19,24 @@ SYSTEM_PROMPT = """
 """
 
 
-def generate_answer(question: str, temperature: Optional[float] = None) -> str:
+def answer(
+    question: str,
+    collection: Collection | None = None,
+    temperature: Optional[float] = None,
+) -> str:
     if temperature is None:
         temperature = 0
 
-    chat_model = ChatOpenAI()
+    if collection is None:
+        chat_model = ChatOpenAI()
 
-    return chat_model.predict(question)
+        return chat_model.predict(question)
+
+    embeddings = OpenAIEmbeddings()
+    client = QdrantClient(path=QDRANT_PATH)
+    qdrant = Qdrant(client, collection_name=collection, embeddings=embeddings)
+
+    qa = RetrievalQA.from_chain_type(
+        llm=OpenAI(), chain_type="stuff", retriever=qdrant.as_retriever()
+    )
+    return qa.run(question)
---
+++ scripts/embed_documents/README.md
@@ -0,0 +1,13 @@
+# Embed documents
+
+## Example usage
+
+```shell
+pipenv run python scripts/embed_documents/embed_documents.py --document-path scripts/samples
+```
+
+To create langchain collection.
+
+```shell
+pipenv run python scripts/embed_documents/embed_documents.py --document-path ../langchain/docs/ --collection-name langchain
+```
